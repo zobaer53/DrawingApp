74e3f4b9a1b0cae006c1131158493230bd7b0a05
zobaer53 <hossain.zobaer11858@gmail.com>
2022-08-05 21:22:13 +0600

    Window manager set

--- app/src/main/java/com/zobaer53/kidsdrawingapp/SplashscreenActivity.kt
+++ app/src/main/java/com/zobaer53/kidsdrawingapp/SplashscreenActivity.kt
@@ -28,6 +28,11 @@ class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        setContentView(R.layout.activity_splashscreen)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
         supportActionBar?.title = ""
         val imageView = findViewById<LottieAnimationView>(R.id.SplashScreenImage)
         imageView.playAnimation()
